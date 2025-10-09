9ea2d7b3811f59768aa89f17756a90c4eaae151d
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-10-09 22:59:39 +0200

    Find All Exits

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -14,8 +14,27 @@ public class Room
     public List<Hallway> CalculateAllPossibleDoorways(int width, int length, int minDistanceFromEdge)
     {
         List<Hallway> hallwayCandidates = new List<Hallway>();
-        hallwayCandidates.Add(new Hallway(new Vector2Int(0, 0)));
-        hallwayCandidates.Add(new Hallway(new Vector2Int(width, length)));
+
+        int bottom = length - 1;
+        int minX = minDistanceFromEdge;
+        int maxX = width - minDistanceFromEdge;
+        
+        int right = width - 1;
+        int minY = minDistanceFromEdge;
+        int maxY = length - minDistanceFromEdge;
+
+        for (int x = minX; x < maxX; x++)
+        {
+            hallwayCandidates.Add(new Hallway(new Vector2Int(x, 0)));
+            hallwayCandidates.Add(new Hallway(new Vector2Int(x, bottom)));
+        }
+
+        for (int y = minY; y < maxY; y++)
+        {
+            hallwayCandidates.Add(new Hallway(new Vector2Int(0, y)));
+            hallwayCandidates.Add(new Hallway(new Vector2Int(right, y)));
+        }
+
         return hallwayCandidates;
     }
 }
