9560702b95714284118efa5dc895bc052cb0566f
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-10-24 15:44:22 +0200

    Create Level Geometry

--- Assets/Scripts/MarchingSquares.cs
+++ Assets/Scripts/MarchingSquares.cs
@@ -3,19 +3,29 @@ using UnityEngine;
 public class MarchingSquares : MonoBehaviour
 {
     [SerializeField] private Texture2D m_levelTexture;
+    [SerializeField] private GameObject m_generatedLevel;
+    [SerializeField] private Tileset m_tileset;
+    [SerializeField] private int m_scale = 1;
+
     [ContextMenu("Create Level Geometry")]
     public void CreateLevelGeometry()
     {
+        m_generatedLevel.transform.DestroyAllChildren();
+        Vector3 scaleVector = new Vector3(m_scale, m_scale, m_scale);
         TextureBasedLevel level = new TextureBasedLevel(m_levelTexture);
         for (int y = 0; y < level.Length - 1; y++)
         {
-            string row = "";
             for (int x = 0; x < level.Width - 1; x++)
             {
                 int tileIndex = CalculateTileIndex(level, x, y);
-                row += tileIndex.ToString("00") + " ";
+                GameObject tilePrefab = m_tileset.GetTile(tileIndex);
+                if (tilePrefab == null) { continue; }
+                GameObject tileInstance = Instantiate(tilePrefab, m_generatedLevel.transform);
+                tileInstance.transform.localScale = scaleVector;
+                tileInstance.transform.position = new Vector3(x * m_scale, 0, y * m_scale);
+                string tileName = "x " + x + " y " + y + " (Index " + tileIndex + ")";
+                tileInstance.name = tileName;
             }
-            Debug.Log(row);
         }
     }
 
@@ -25,7 +35,7 @@ public class MarchingSquares : MonoBehaviour
         int topRight = level.IsBlocked(x + 1, y + 1) ? 1 : 0;
         int bottomLeft = level.IsBlocked(x, y) ? 1 : 0;
         int bottomRight = level.IsBlocked(x + 1, y) ? 1 : 0;
-        int tileIndex = topLeft + (topRight << 1) + (bottomRight << 2) + (bottomLeft << 3);
+        int tileIndex = topLeft + (topRight << 1) + (bottomLeft << 2) + (bottomRight << 3);
         return tileIndex;
     }
 }
