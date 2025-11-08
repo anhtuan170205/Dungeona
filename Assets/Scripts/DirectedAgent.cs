56271dba6abb5dfc43339f488083f1af57d066ed
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-11-08 07:34:23 +0100

    Animate Player

--- Assets/Scripts/DirectedAgent.cs
+++ Assets/Scripts/DirectedAgent.cs
@@ -3,15 +3,20 @@ using UnityEngine.AI;
 
 public class DirectedAgent : MonoBehaviour
 {
-    NavMeshAgent _agent;
+    private NavMeshAgent _agent;
+    private Animator _animator;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
     }
     private void Update()
     {
-
+        Vector3 velocity = _agent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        _animator.SetFloat("forwardSpeed", speed);
     }
     
     public void GoToLocation(Vector3 destination)
